169fbdd9a1970c3435b82f4c2884faf8d1f32b92
Ross Johnson <RossJ24@users.noreply.github.com>
2021-03-13 13:12:27 -0500

    Add Startup card base and Page views

---
+++ src/components/StartupCard/StartupCard.tsx
@@ -0,0 +1,14 @@
+import React from  'react';
+import StartUpCardProps from '../../models/StartUpCardProps';
+
+export default class StartUp extends React.Component<StartUpCardProps>{
+    
+
+    render(){
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
