4693f21100977e0e8322ec4807c1ef31021c9504
Ross Johnson <RossJ24@users.noreply.github.com>
2021-03-19 16:07:30 -0400

    Add finishing touches

--- src/components/Header/Navbar/Navbar.tsx
+++ src/components/Header/Navbar/Navbar.tsx
@@ -12,12 +12,12 @@ class NavBar extends React.Component<{}, CurrentPage>{
                 <a className="nav-link" href="/startups">
                     Startups
                 </a>
-                <a className="nav-link" href="/fundraising">
+                {/* <a className="nav-link" href="/fundraising">
                     Fundraising this month
                 </a>
                 <a className="nav-link" href="/mentorship">
                     Looking for mentorship
-                </a>
+                </a> */}
                 <a className="nav-link" id="last-nav-link" href="/venture-capital">
                     VC Team
                 </a>
