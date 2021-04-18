518b02869d80930185a3b3682b83b1ee5d9e0ffa
Cesar Lara <cesar.lara@yale.edu>
2021-04-18 13:59:04 -0500

    adding header changes

--- src/components/Header/BurgerMenu/BurgerMenu.tsx
+++ src/components/Header/BurgerMenu/BurgerMenu.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 export default class HamburgerMenu extends React.Component<{}>{
     render(){
         return(
-            <div>
-                
+            <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
             </div>
+            
         );
     }
 }
