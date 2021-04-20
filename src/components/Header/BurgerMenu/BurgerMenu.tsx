3e72bbfef2150c422d2012917f0c9bb05356654f
Cesar Lara <cesar.lara@yale.edu>
2021-04-20 18:40:24 -0500

    adding header

--- src/components/Header/BurgerMenu/BurgerMenu.tsx
+++ src/components/Header/BurgerMenu/BurgerMenu.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
+import { MenuItems } from "./MenuItems"
+import './BurgerMenu.css'
 
-export default class HamburgerMenu extends React.Component<{}>{
-    render(){
+class Navbar extends React.Component {
+    state = { clicked: false }
+
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+
+    }
+
+
+    render() {
         return(
-            <div className="menu-icon" onClick={this.handleClick}>
+            <nav className="NavbarItems">
+                <h1 className="navbar-logo">YES Startup Incubator</h1>
+
+                <div className="menu-icon" onClick={this.handleClick}>
                     <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-            </div>
-            
-        );
+                </div>
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}>
+                                {item.title}
+                                </a>
+                            </li>
+                        )
+                    })}  
+                </ul>
+            </nav>
+
+
+        )
     }
 }
+
+
+
+export default Navbar
---
+++ src/components/Header/BurgerMenu/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+        title: 'Non-Profit Founders',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Startup Founders',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Partnerships',
+        url: '#',
+        cName: 'nav-links'
+    }
+]
