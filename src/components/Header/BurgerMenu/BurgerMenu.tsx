4849ebee13b61d1a39818b44c4f7faf984d43d14
Ross Johnson <RossJ24@users.noreply.github.com>
2021-04-30 11:52:18 -0400

    Move VC to views and make BurgerMenu Functional

--- src/components/Header/BurgerMenu/BurgerMenu.tsx
+++ src/components/Header/BurgerMenu/BurgerMenu.tsx
@@ -1,48 +1,38 @@
 import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
 import './BurgerMenu.css'
-import Logo from '../../../images/Logo.png'
 
-class BurgerMenu extends React.Component<{}, any> {
+const BurgerMenu = () => {
 
-    constructor(props: any) {
-        super(props);
-
-        this.state = { clicked: false }
-    }
-
-
-    render() {
-        return (
-            <Menu
-                noOverlay
-                disableAutoFocus
-                isOpen={false}
-                right={true}
-            >
-                <a className="menu-item" href="/">
-                    <p>
-                        Home
+    return (
+        <Menu
+            noOverlay
+            disableAutoFocus
+            isOpen={false}
+            right={true}
+        >
+            <a className="menu-item" href="/">
+                <p>
+                    Home
                     </p>
 
-                </a>
-                <a className="menu-item" href="/startups">
+            </a>
+            <a className="menu-item" href="/startups">
 
-                    <p>
-                        <span className="replace-s">S</span>tartup<span className="replace-s">S</span>
+                <p>
+                    <span className="replace-s">S</span>tartup<span className="replace-s">S</span>
+                </p>
+            </a>
+            <a className="menu-item" href="/venture-capital">
+                <p>
+                    VC Team
                     </p>
-                </a>
-                <a className="menu-item" href="/venture-capital">
-                    <p>
-                        VC Team
-                    </p>
 
-                </a>
-            </Menu>
+            </a>
+        </Menu>
 
 
-        )
-    }
+    );
 }
 
 
